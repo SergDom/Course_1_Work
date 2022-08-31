222af1161d94111022bcc1a8dbeebfcc0e70798b
sergeydomanov <nomad-labs@yandex.ru>
2022-08-31 20:05:10 +0300

    New commit

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,29 @@
 public class Employee {
+    private String lastName;
+    private String firstName;
+    private String surName;
+
+    private int department;
+    private int salary;
+    static private int id;
+
+    public Employee (String lastName, String firstName, String surName, int department, int salary) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.surName = surName;
+        this.department = department;
+        this.salary=salary;
+    }
+    public String getLastName() {return lastName;}
+    public String getFirstName() {return firstName;}
+    public String getSurName() {return surName;}
+
+    public int getDepartment() {return department;}
+    public void setDepartment(int department) {this.department = department;}
+
+    public int getSalary() {return salary;}
+    public void setSalary(int salary) {this.salary = salary;}
+
+
 }
+
