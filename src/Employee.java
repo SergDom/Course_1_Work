9104a19f39b8e83d7dfafacd1e22d85fde122e52
sergeydomanov <nomad-labs@yandex.ru>
2022-08-31 20:56:27 +0300

    New commit

--- src/Employee.java
+++ src/Employee.java
@@ -13,6 +13,7 @@ public class Employee {
         this.surName = surName;
         this.department = department;
         this.salary=salary;
+        id++
     }
     public String getLastName() {return lastName;}
     public String getFirstName() {return firstName;}
@@ -24,6 +25,10 @@ public class Employee {
     public int getSalary() {return salary;}
     public void setSalary(int salary) {this.salary = salary;}
 
-
+    @Override
+    public String toString() {
+        return "Сотрудник: " + "Фамилия: " + lastName + " Имя: " + firstName + " Отчество: " + surName + " Отдел №: " + department +
+                " Зарплата: " + salary;
+    }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+//    int Employee[] = new int[10];
 
+        Employee employee1 = new Employee ("Ivanov", "Ivan", "Ivanovich", 1, 1500);
+
+        System.out.println(employee1);
+        System.out.println(employee1);
 
     }
 }
