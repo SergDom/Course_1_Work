38750c5cbe9c07be9063dc218438cfac81da000b
sergeydomanov <nomad-labs@yandex.ru>
2022-09-04 16:02:03 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 public class  Main {
-    static Employee[] employees = new Employee[10];
 
 
-    public static void sumOfSalary(Employee employee) {
-            int sum = 0;
-        for (int i = 0; i < employees.length; i++){
-          sum += employees[i].getSalary();}
-          System.out.println(sum);
+    public static int sumOfSalary(Employee [] arr) {
+        int sumSalary = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                sumSalary = sumSalary + arr[i].getSalary();
+            } else break;}
+            return sumSalary;
+        }
 
-    }
 //    public static int minOfSalary(Employee[] salary) {
 //        int salary[] = new Employee().getSalary();
 //        int min=0;
@@ -33,6 +34,7 @@ public class  Main {
 
         public static void main (String[]args){
 
+           Employee[] employees = new Employee[10];
 
 //            public static int listOfEmployee (Employee employee){
 //                for (int i = 0; i < employees.length - 1; i++) {
@@ -41,19 +43,28 @@ public class  Main {
 //                        break;
 //                }
 //            }
-            employees[0] = new Employee("Ivanov", "Ivan", "Ivanovich", 1, 150000);
-            employees[1] = new Employee("Dmitriev", "Sergey", "Aleksandrovich", 2, 160000);
-            employees[2] = new Employee("Seleznev", "Aleksander", "Alekseevich", 3, 145000);
-            employees[3] = new Employee("Petrov", "Ilia", "Sergeevich", 4, 180000);
+            Employee employee1 = new Employee("Ivanov", "Ivan", "Ivanovich", 1, 150000);
+            Employee employee2 = new Employee("Dmitriev", "Sergey", "Aleksandrovich", 2, 160000);
+            Employee employee3 = new Employee("Seleznev", "Aleksander", "Alekseevich", 3, 145000);
+            Employee employee4 = new Employee("Petrov", "Ilia", "Sergeevich", 4, 180000);
 
-//            for (int i = 0; i < employee.length - 1; i++)
-//                if (employee[i] != null)
-//                    System.out.println(employee[i]);
+
+
+            employees[0] = employee1;
+            employees[1] = employee2;
+            employees[2] = employee3;
+            employees[3] = employee4;
+
+//            toString(employees);
+
+            for (int i = 0; i < employees.length - 1; i++)
+                if (employees[i] != null)
+                    System.out.println(employees[i]);
 
 //            System.out.println(listOfEmployee());
             System.out.println("Количество сотрудников: " + Employee.id);
 
-//            System.out.println(minOfSalary(employee));
+            System.out.println("Сумма затрат по зарплате: " + sumOfSalary(employees));
 
 //            sumOfSalary(Employee(employees[i].getSalary()));
 
