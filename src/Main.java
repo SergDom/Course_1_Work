359b7bd2f13c0353f237b0a622541d8f2072fa5c
sergeydomanov <nomad-labs@yandex.ru>
2022-09-05 20:34:56 +0300

    New commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class  Main {
 
-
+    public static void listOfEmployee (Employee[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                System.out.println((arr[i]));}
+            }
+        }
     public static int sumOfSalary(Employee [] arr) {
         int sumSalary = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -31,15 +36,14 @@ public class  Main {
             return max;
         }
 
-    public static double avrOfSalary(Employee [] arr) {
-        double sumSalary = 0;
-        for (int i = 0, j=0; i < arr.length; i++, j++) {
-            if (arr[j] != null) {
-                sumSalary = (sumSalary + arr[j].getSalary());
-            }
-            return sumSalary / arr.length;}
-            return sumSalary;
-        }
+    public static double avrOfSalary(Employee[] arr) {
+        double avrSalary = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != null) {
+                avrSalary = avrSalary + arr[i].getSalary()/arr.length;
+            } else break;}
+        return avrSalary;
+    }
 
     public static void employeeNames (Employee [] arr) {
         for (int i = 0; i < arr.length; i++)
@@ -57,18 +61,26 @@ public class  Main {
             Employee employee2 = new Employee("Dmitriev", "Sergey", "Aleksandrovich", 2, 160000);
             Employee employee3 = new Employee("Seleznev", "Aleksander", "Alekseevich", 3, 145000);
             Employee employee4 = new Employee("Petrov", "Ilia", "Sergeevich", 4, 180000);
-
+            Employee employee5 = new Employee("Vasechkin", "Anton", "Genadeivich", 5, 170000);
+            Employee employee6 = new Employee("Ivanova", "Ylia", "Viktorovna", 5, 185000);
+            Employee employee7 = new Employee("Osipova", "Galina", "Sergeevna", 2, 143000);
+            Employee employee8 = new Employee("Smirnova", "Sofia", "Dmitreevna", 1, 167000);
+            Employee employee9 = new Employee("Fedorova", "Diana", "Vladimirovna", 3, 125000);
+            Employee employee10 = new Employee("Pavlova", "Elena", "Aleksandrovna", 4, 171000);
 
             employees[0] = employee1;
             employees[1] = employee2;
             employees[2] = employee3;
             employees[3] = employee4;
+            employees[4] = employee5;
+            employees[5] = employee6;
+            employees[6] = employee7;
+            employees[7] = employee8;
+            employees[8] = employee9;
+            employees[9] = employee10;
 
 
-            for (int i = 0; i < employees.length - 1; i++)
-                if (employees[i] != null)
-                    System.out.println(employees[i]);
-
+            listOfEmployee(employees);
             System.out.println("Количество сотрудников: " + Employee.id);
             System.out.println("==================================");
             System.out.println("Сумма затрат по зарплате: " + sumOfSalary(employees));
